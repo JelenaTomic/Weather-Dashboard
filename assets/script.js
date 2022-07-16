eb1475f54a7f9b2b6f19df7ca1b809f033109473
JelenaTomic <tomic.elena09@gmail.com>
2022-07-15 20:31:53 -0500

    added var in js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // ajax request 
+    var cityVar = "";
+    const api_key = "e14cf45a50592027f0ccce5ea6982a48";
+    var weatherURL;
+    var forecastURL;
+    var date = moment().format('MMM Do YY');
+    var history;
+    var cityName;
+    var $forecast = document.querySelector(".card");
+})
